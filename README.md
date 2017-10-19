57e8d4ded861a04f26f2a23e9b948707dc47ff10
zheng_c <zheng_c@worksap.co.jp>
2017-10-19 15:10:09 +0800

    generate a spring-boot project

---
+++ README.md
@@ -0,0 +1,4 @@
+Generate project using Intellij IDEA
+=========
+* create a new project, choosing spring Initializr
+* Choose web -> web in dependency page
---
+++ src/main/java/com/chiucheng/timestampgen/TimestampGenApplication.java
@@ -0,0 +1,12 @@
+package com.chiucheng.timestampgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimestampGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimestampGenApplication.class, args);
+	}
+}
